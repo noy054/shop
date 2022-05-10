9349376a173b52d2d011a1a3f2c0175151241fb3
noy <noy05464@gmail.com>
2022-05-10 16:27:23 +0300

    dcf

--- src/routers/checkout/checkout.component.jsx
+++ src/routers/checkout/checkout.component.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-import { selectCart, selectNewTotal} from '../../store/cart/cart.selector'
+import { selectCart, selectNewTotal } from "../../store/cart/cart.selector";
 
-import {CheackoutContainer, CheckoutHeader, HeaderBlock, Total} from './checkout.styles.jsx'
+import {
+  CheackoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total,
+} from "./checkout.styles.jsx";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
-import PaymentForm from '../../components/payment-form/payment-form-component';
-
+import PaymentForm from "../../components/payment-form/payment-form-component";
 
 const CheckOut = () => {
-    const  cartItems = useSelector(selectCart);
-    const cartTotal = useSelector(selectNewTotal);
-    return (
-        <CheackoutContainer>
-           < CheckoutHeader>
-              <HeaderBlock>
-                    <span>Product</span> 
-              </HeaderBlock>
-              <HeaderBlock>
-                    <span>Description</span> 
-              </HeaderBlock>
-              <HeaderBlock >
-                    <span>Quantity</span> 
-              </HeaderBlock>
-              <HeaderBlock>
-                    <span>Price</span> 
-              </HeaderBlock>
-              <HeaderBlock>
-                    <span>Remove</span> 
-              </HeaderBlock>
-          </CheckoutHeader>
-              {
-                  cartItems.map((cartItem) => (
-                      <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
-                  ))
-              }
-              <Total>Total: ${cartTotal}</Total>
-              <PaymentForm></PaymentForm>
-          </CheackoutContainer>
-    );
-}
+  const cartItems = useSelector(selectCart);
+  const cartTotal = useSelector(selectNewTotal);
+  return (
+    <CheackoutContainer>
+      <CheckoutHeader>
+        <HeaderBlock>
+          <span>Product</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Description</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Quantity</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Price</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Remove</span>
+        </HeaderBlock>
+      </CheckoutHeader>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <Total>Total: ${cartTotal}</Total>
+
+      <PaymentForm />
+    </CheackoutContainer>
+  );
+};
 
 export default CheckOut;
--- src/routers/shop/shop.component.jsx
+++ src/routers/shop/shop.component.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React from "react";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
-import { fetchCategoriesStart } from '../../store/categories/category.action';
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 import CategoriesPreview from "../cetegories-preview/categories-peview.component";
 import Cetegory from "../cetegory/cetegory.componenet";
-
-
-
-import './shop.styles.scss'
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-        dispatch(fetchCategoriesStart());
-   }, []);
-   
-    return(
-      <Routes>
-        <Route index element={<CategoriesPreview/>}/> 
-        <Route path=":cetegory" element={<Cetegory/>}/>
-      </Routes> 
-    );
+    dispatch(fetchCategoriesStart());
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":cetegory" element={<Cetegory />} />
+    </Routes>
+  );
 };
 
 export default Shop;
