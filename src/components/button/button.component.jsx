425b13637b29b91e422bff856778b48c9cb229be
noy <noy05464@gmail.com>
2022-05-12 10:06:50 +0300

    dcsd

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import {BaseButton, GoogleSingInButton, InvertedButton} from './button.styles.jsx'
+import React from "react";
+import {
+  BaseButton,
+  GoogleSingInButton,
+  InvertedButton,
+  LoadingSpinner,
+} from "./button.styles.jsx";
 
-export const BUTTON_TYPE_CLASSES ={
-    base: 'base',
-    google: 'google-sign-in',
-    inverted: 'inverted'
-}
+export const BUTTON_TYPE_CLASSES = {
+  base: "base",
+  google: "google-sign-in",
+  inverted: "inverted",
+};
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => ({
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  ({
     [BUTTON_TYPE_CLASSES.base]: BaseButton,
-    [BUTTON_TYPE_CLASSES.google]:GoogleSingInButton, 
+    [BUTTON_TYPE_CLASSES.google]: GoogleSingInButton,
     [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-}[buttonType])
+  }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps}) => {
-    const CustButton = getButton(buttonType)
-    return (
-        <CustButton {...otherProps}>{children}</CustButton>
-    );
-}
+const Button = ({ children, buttonType, isLoading = false, ...otherProps }) => {
+  const CustButton = getButton(buttonType);
+  return (
+    <CustButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <LoadingSpinner /> : children}
+    </CustButton>
+  );
+};
 
 export default Button;
